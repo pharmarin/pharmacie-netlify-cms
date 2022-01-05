8663ca3462db6e681ca59655a71f766f2edff70a
Marin ROUX <marin@Mac-mini-de-Marin.local>
2022-01-05 18:52:25 +0100

    🐛 FIX: Fix netlify-cms not building TSX

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,13 @@ module.exports = {
       "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
   },
   plugins: [
+    {
+      resolve: "gatsby-plugin-typescript",
+      options: {
+        isTSX: true,
+        allExtensions: true,
+      },
+    },
     "gatsby-plugin-resolve-src",
     "gatsby-plugin-graphql-codegen",
     "gatsby-plugin-postcss",
